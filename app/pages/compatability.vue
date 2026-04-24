e5f96e5432bc4783e0c7a6f3a6d974e459e1582c
wiz-rd <126345400+wiz-rd@users.noreply.github.com>
2026-04-24 13:40:14 -0400

    Added some Markdown pages and preliminary support
    We're attempting to use Markdown for our wiki pages, so I added a few Markdown pages and attempted to add support for them per https://content.nuxt.com/docs/files/markdown , unable to test it at the moment.

---
+++ app/pages/compatability.vue
@@ -0,0 +1,15 @@
+<script setup>
+const compatability = useRoute().params.compatability
+const { data: page } = await useAsyncData(`${compatability}`, () => {
+    // See faq.vue for `.first()` thoughts.
+  return queryCollection('content').path(`/compatability`).first()
+})
+</script>
+
+<template>
+  <!--
+  Reference for this code:
+  https://content.nuxt.com/docs/files/markdown#display-markdown
+  -->
+  <ContentRenderer :value="page" />
+</template>
--- app/pages/faq.vue
+++ app/pages/faq.vue
@@ -1,11 +1,16 @@
-<script setup lang="ts">
-
+<script setup>
+const faq = useRoute().params.faq
+const { data: page } = await useAsyncData(`${faq}`, () => {
+  // Not 100% sure we have to get the first here, but I think
+  // queryCollection returns a list, so we do need to get a single element.
+  return queryCollection('content').path(`/faq`).first()
+})
 </script>
 
 <template>
-  <h1>FAQ</h1>
+  <!--
+  Reference for this code:
+  https://content.nuxt.com/docs/files/markdown#display-markdown
+  -->
+  <ContentRenderer :value="page" />
 </template>
-
-<style scoped>
-
-</style>
