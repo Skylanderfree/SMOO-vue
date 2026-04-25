b518beda781499deff2e084691cad3576c4adbc8
wiz-rd <126345400+wiz-rd@users.noreply.github.com>
2026-04-25 10:18:28 -0400

    Update faq.vue
    Added lang="ts"

--- app/pages/faq.vue
+++ app/pages/faq.vue
@@ -1,4 +1,4 @@
-<script setup>
+<script setup lang="ts">
 const faq = useRoute().params.faq
 const { data: page } = await useAsyncData(`${faq}`, () => {
   // Not 100% sure we have to get the first here, but I think
